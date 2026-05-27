9bd38a35a17d1ca31ab90dfe595ca3ca07153ba6
Yogeshwaran <suyogesh06@gmail.com>
2026-05-27 15:44:01 +0530

    admin auto create

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,12 +1,13 @@
 from django.apps import AppConfig
 
-
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
     def ready(self):
-        from django.contrib.auth.models import User
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
 
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
