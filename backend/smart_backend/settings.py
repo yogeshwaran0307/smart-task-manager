acac993d52258bbf629a3b4fcc1017e43a220c18
Yogeshwaran <suyogesh06@gmail.com>
2026-06-02 17:52:49 +0530

    fix: TiDB SSL settings

--- backend/smart_backend/settings.py
+++ backend/smart_backend/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
 
         'OPTIONS': {
-            'ssl': {'ca': None},
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'ssl_mode': 'REQUIRED',
         },
     }
 }
